8bd1df67b8884b2e8860d9ee207607ea93176a9b
Othmane Agrram <oagrram@e2r3p14.1337.ma>
2019-05-24 20:39:45 +0000

    24 May 2019

--- fillit.c
+++ fillit.c
@@ -3,76 +3,103 @@
 /*                                                        :::      ::::::::   */
 /*   fillit.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oagrram <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: aymhabib <aymhabib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/12 01:12:57 by oagrram           #+#    #+#             */
-/*   Updated: 2019/05/12 01:12:58 by oagrram          ###   ########.fr       */
+/*   Created: 2018/12/13 14:40:40 by aymhabib          #+#    #+#             */
+/*   updated: 2019/05/22 23:55:36 by oagrram          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static int	fillit(int fd)
+int		tet_nb(char *file)
 {
- 	int i;
-    char *line;
-    char *tmpter;
-    int c;
-    //char **teriminos;
-    int j = 0;
+	int i;
+	int nb;
 
-	c = 0; 
-    i = 0;
-   tmpter = ft_strdup("");
-	while (get_next_line(fd, &line) > 0 )
-    {
-		tmpter = ft_strjoin(tmpter, line);
-        //printf("i == %d\n",i);
-        //printf("(line[%d] == %s\n",i,tmpter);
-      	printf("tmpter == %s\n",tmpter);
-        printf("i == %d\n",i);
-        if(++i == 5)
-        {
-        	printf("tmpter == %s\n",tmpter);
-        	while(j++ < 16)
-        	{
-        	     printf("j == %d\n",j);
-				if(tmpter[j] == '\0')
-				{
-					tmpter[j] = '\n';
-					printf("tmpter[%d] == %s\n",j,tmpter);
-				}
-        	}
-        	j = 0;
-        	
-        	c++;
-            if(!check_if_valid(tmpter))
-            {
-                printf("invalid\n");
-                return (0);
-            }
-          //  get_next_line(fd, &line);
-            printf("(line) == %s\n",line);
-            printf("strlen (line) == %d\n",(int)ft_strlen(line));
-            if(ft_strlen(line))
-            {
-            	printf("empty line\n");
-            	return 0;
-            }
-            tmpter = ft_strdup("");
-            i = 0;
-        }
-    }
-   // if(c > 26 || )
-    printf("valid\n");
-    return 0;
+	i = 0;
+	nb = 1;
+	while (file[i])
+	{
+		if (file[i + 1] == '\n' && file[i] == '\n')
+			nb++;
+		i++;
+	}
+	return (nb);
 }
 
-int	main (int argc, char **argv)
-{   
-    int fd;
+char	*read_file(int fd)
+{
+	char			buff[BUFF_SIZE + 1];
+	char			*file;
+	char			*tmp;
+	int				i;
 
-	argc = 1;
-    fd = open(*(argv + 1) , O_RDONLY);
-    return	(fillit(fd));
+	i = 0;
+	file = ft_strnew(0);
+	while ((i = read(fd, buff, BUFF_SIZE)))
+	{
+		buff[i] = '\0';
+		tmp = ft_strjoin(file, buff);
+		free(file);
+		file = ft_strdup(tmp);
+		free(tmp);
+	}
+	return (file);
+}
+
+char	***if_valid(int nb, char **tetriminos)
+{
+	int		i;
+	char	***tetris;
+
+	tetris = (char ***)malloc(sizeof(**tetris) * (nb + 1));
+	//printf("sizeof(tetris) == %lu\n",(sizeof(**tetris) * (nb + 1)));
+	if (!tetris)
+		return (NULL);
+	i = 0;
+	while (i < nb)
+	{
+	//	printf("tetriminos[%d] ==  \n%s",i,tetriminos[i]);
+		tetris[i] = ft_spliiiit(tetriminos[i], 'A' + i);
+		i++;
+	}
+	tetris[nb] = NULL;
+	ft_free(tetriminos);
+	return (tetris);
+}
+
+void	error(void)
+{
+	ft_putstr("error\n");
+	exit(42);
+}
+
+int		main(int argc, char **argv)
+{
+	int				fd;
+	char			**tetriminos;
+	char			*file;
+	int				nb;
+	char			***tetris;
+
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 0 || (read(fd, NULL, 0) < 0))
+			error();
+		file = read_file(fd);
+		nb = tet_nb(file);
+		tetriminos = split(file, nb);
+		if (validating(file, tetriminos, nb))
+		{
+			tetris = if_valid(nb, tetriminos);
+			ft_solve(tetris, nb);
+		}
+		else
+			error();
+	}
+	else
+		ft_putstr("usage: ./fillit source_file\n");
+	return (0);
 }
